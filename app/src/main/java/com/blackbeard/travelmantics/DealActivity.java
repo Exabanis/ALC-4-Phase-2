1fbdc244a9ec8a5576f5b6a637493d55b0230512
exabanis <xabanisat2webmail.co.za>
2019-08-07 15:59:08 +0200

    first commit

---
+++ app/src/main/java/com/blackbeard/travelmantics/DealActivity.java
@@ -0,0 +1,203 @@
+package com.exabanis.travelmantics;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+public class DealActivity extends AppCompatActivity {
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private static final int PICTURE_RESULT = 42;
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
+    ImageView imageView;
+    TravelDeal deal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deal);
+
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrice);
+        imageView = findViewById(R.id.image);
+        Intent intent = getIntent();
+        TravelDeal deals = (TravelDeal) intent.getSerializableExtra("Deal");
+        if (deals == null)
+        {
+            deals = new TravelDeal();
+        }
+        this.deal = deals;
+        txtTitle.setText(deals.getTitle());
+        txtDescription.setText(deals.getDescription());
+        txtPrice.setText(deals.getPrice());
+        showImage(deals.getImageUrl());
+        Button btnImage = findViewById(R.id.btnImage);
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(intent.createChooser(intent, "Insert Picture"), PICTURE_RESULT);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal Saved", Toast.LENGTH_LONG).show();
+                clean();
+                backToList();
+                return true;
+            case R.id.delete_deal:
+                deleteDeal();
+                Toast.makeText(this, "Deal Deleted", Toast.LENGTH_LONG).show();
+                backToList();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        if (FirebaseUtil.isAdmin)
+        {
+            menu.findItem(R.id.delete_deal).setVisible(true);
+            menu.findItem(R.id.save_menu).setVisible(true);
+            enableEditTexts(true);
+        }
+        else
+        {
+            menu.findItem(R.id.save_menu).setVisible(false);
+            menu.findItem(R.id.delete_deal).setVisible(false);
+            enableEditTexts(false);
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICTURE_RESULT && resultCode == RESULT_OK)
+        {
+            Uri imageUri = data.getData();
+            final StorageReference reference = FirebaseUtil.mStorageRef.child(imageUri.getLastPathSegment());
+            reference.putFile(imageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    String url = reference.getDownloadUrl().toString();//taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                    String pictureName = taskSnapshot.getStorage().getPath();
+                    deal.setImageUrl(url);
+                    deal.setImageName(pictureName);
+                    Log.d("Url:", url);
+                    Log.d("Name: ", pictureName);
+                    showImage(url);
+                }
+            });
+
+        }
+    }
+
+    private void showImage(String url) {
+        if (url != null && url.isEmpty() == false)
+        {
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+            Picasso.with(this)
+                    .load(url)
+                    .resize(width, width*2/3)
+                    .centerCrop()
+                    .into(imageView);
+        }
+    }
+
+    private void enableEditTexts(boolean isEnabled) {
+        txtTitle.setEnabled(isEnabled);
+        txtDescription.setEnabled(isEnabled);
+        txtPrice.setEnabled(isEnabled);
+    }
+
+    private void deleteDeal() {
+        if (deal == null)
+        {
+            Toast.makeText(this, "Please save a deal before deleting", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mDatabaseReference.child(deal.getId()).removeValue();
+        Log.d("Image name", deal.getImageName());
+        if (deal.getImageName() != null && !deal.getImageName().isEmpty())
+        {
+            StorageReference picRef = FirebaseUtil.mStorage.getReference().child(deal.getImageName());
+            picRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.d("Delete Image", "Image Successfully deleted");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("Delete Image", e.getMessage());
+                }
+            });
+        }
+    }
+
+    private void backToList() {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTitle.requestFocus();
+    }
+
+    private void saveDeal() {
+        deal.setTitle(txtTitle.getText().toString());
+        deal.setDescription(txtDescription.getText().toString());
+        deal.setPrice(txtPrice.getText().toString());
+        if (deal.getId() == null)
+        {
+            mDatabaseReference.push().setValue(deal);
+        }
+        else
+        {
+            mDatabaseReference.child(deal.getId()).setValue(deal);
+        }
+    }
+}
